c77000727dbe348f8e5e2cd50fa3d7dbd3b893c4
Mohamed Aiman <muhammadhu.aiman@gmail.com>
2019-02-23 21:45:38 +0500

    modified marker between function to enable search start from 0 position

--- src/Generator/Markers/Marker.php
+++ src/Generator/Markers/Marker.php
@@ -27,7 +27,7 @@ class Marker
 	{
 		$string = ($content) ? $content : $this->content;
 	    $ini = strpos($string, $start);
-	    if ($ini == 0) return '';
+	    // if ($ini == 0) return '';
 	    $ini += strlen($start);
 	    $len = strpos($string, $end, $ini) - $ini;
 	    return substr($string, $ini, $len);
